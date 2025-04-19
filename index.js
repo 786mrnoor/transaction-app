ed1be8ff609124218bfcd6927f4603d7f669ef02
Noor Mohammad <786mrnoor@gmail.com>
2025-04-19 12:06:04 +0530

    fix

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ import transactionRouter from './routes/transaction.js';
 connectDB();
 const app = express();
 
-app.use(express.static(path.join(__dirname, process.env.MY_BUILD_DIR)));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 app.use(cookieParser());
 app.use(express.json());
 app.use('/api/auth/', authRouter);
 app.use('/api/balance', authenticate, balanceRouter);
 app.use('/api/categories', authenticate, categoriesRouter);
 app.use('/api/transactions', authenticate, transactionRouter);
-app.use('*', (req, res) => res.sendFile(path.join(__dirname, process.env.MY_BUILD_DIR, 'index.html')));
+app.use('*', (req, res) => res.sendFile(path.join(__dirname, 'frontend/build', 'index.html')));
 
 
 // app.listen(3001, ()=>{
